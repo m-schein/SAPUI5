0995dbb4deb92da1b185c510065c273f56ff0de4
Maiara Trevisol <maiara.trevisol@sap.com>
2021-06-09 11:41:26 -0300

    input-test

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,6 +1,7 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
+	"sap/m/MessageToast",
+	"sap/m/Page"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -9,35 +10,51 @@ sap.ui.define([
 		"use strict";
 
 		return Controller.extend("project1.controller.View1", {
-			onInit: function () {
+			onInit: () => {
 				console.log("On Init")
 			},
-			onClick: () =>{
-				MessageToast.show('Button is clicked')
+			onClick: () => {
+				var name = document.getElementById("container-project1---View1--productInput-inner").value;
+
+				if(name === ''){
+					MessageToast.show('Any name inserted')
+				}else{
+					MessageToast.show('this is the name: ' + name)
+				}
 			},
+			onPress: () =>{
+				MessageToast.show('there aren\'t a second page yet')
+				//Controller.goToSecondPage()
+			},
+
+			/*goToSecondPage: function(oEvent){
+
+			},*/
+
 			/**
 			 * @override
 			 */
 			/*onBeforeRendering: function() {
 				Controller.prototype.onBeforeRendering.apply(this, arguments);
-				
-			
 			},
 			/**
 			 * @override
 			 */
 			onAfterRendering: function() {
 				Controller.prototype.onAfterRendering.apply(this, arguments);
-				
-			
 			},
 			/**
 			 * @override
 			 */
 			onExit: function() {
 				Controller.prototype.onExit.apply(this, arguments);
-				
-			
 			}
 		});
+		/*createContent: function(Controller){
+			var oSecondPage = new sap.m.Page()({
+				title: 'secondPage'
+			})		
+			return oSecondPage
+		}*/
+		
 	});
